0b62dbef420cc23e4e8b049db30b481f96532cac
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-07-17 21:40:50 +0300

    feat: add array.prototype.filter

---
+++ test/fixtures/array.prototype.filter/case-1/before.js
@@ -0,0 +1,15 @@
+var filter = require("array.prototype.filter");
+var assert = require("assert");
+
+assert.deepEqual(
+  filter([1, 2, 3], function (x) {
+    return x >= 2;
+  }),
+  [2, 3],
+);
+assert.deepEqual(
+  filter([1, 2, 3], function (x) {
+    return x <= 2;
+  }),
+  [1, 2],
+);

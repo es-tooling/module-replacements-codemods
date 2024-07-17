bd8002918563a6f4dfad9a802ae6fb76538b8791
thepassle <pascalschilp@gmail.com>
2024-07-17 19:02:23 +0200

    chore: format

--- codemods/index.js
+++ codemods/index.js
@@ -11,10 +11,10 @@ import isNumberObject from './is-number-object/index.js';
 import isString from './is-string/index.js';
 
 export const codemods = {
-    'is-whitespace': isWhitespace,
-    'is-array-buffer': isArrayBuffer,
-    'is-boolean-object': isBooleanObject,
-    'is-date-object': isDateObject,
-    'is-number-object': isNumberObject,
-    'is-string': isString
+	'is-whitespace': isWhitespace,
+	'is-array-buffer': isArrayBuffer,
+	'is-boolean-object': isBooleanObject,
+	'is-date-object': isDateObject,
+	'is-number-object': isNumberObject,
+	'is-string': isString,
 };

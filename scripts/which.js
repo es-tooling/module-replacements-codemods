353bec1e9d7cabf1429698d6c35341954fb9567a
thepassle <pascalschilp@gmail.com>
2024-07-19 13:14:16 +0200

    fix: exclude preferred modules for now, theyre a bit more complex and not very straightforward to codemod

--- scripts/which.js
+++ scripts/which.js
@@ -1,11 +1,13 @@
-import { all } from 'module-replacements';
+import { nativeReplacements, microUtilsReplacements } from 'module-replacements';
 import { codemods } from '../codemods/index.js';
+
+const all = [...nativeReplacements.moduleReplacements, ...microUtilsReplacements.moduleReplacements];
 
 /**
  * Figure out which module replacements are not implemented as codemods yet
  */
 
-const filteredModuleReplacements = all.moduleReplacements.filter(
+const filteredModuleReplacements = all.filter(
 	(replacement) => !Object.keys(codemods).includes(replacement.moduleName),
 );
 
@@ -15,13 +17,13 @@ filteredModuleReplacements.forEach((replacement) => {
 
 const implementedCodemods = Object.keys(codemods).length; // Number of implemented codemods
 const implementedCodemodsPercentage =
-	(implementedCodemods / all.moduleReplacements.length) * 100;
+	(implementedCodemods / all.length) * 100;
 
 console.log(
 	`\nNumber of implemented codemods: ${Object.keys(codemods).length}`,
 );
 console.log(
-	`Total number of module replacements: ${all.moduleReplacements.length}`,
+	`Total number of module replacements: ${all.length}`,
 );
 console.log(
 	`Number of module replacements left to implement codemods for: ${filteredModuleReplacements.length}`,

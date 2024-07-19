472ee7868fdccddba4d1560af533f4378ece96c0
thepassle <pascalschilp@gmail.com>
2024-07-19 11:58:37 +0200

    chore: format

--- codemods/is-builtin-module/index.js
+++ codemods/is-builtin-module/index.js
@@ -1,4 +1,4 @@
-import jscodeshift from "jscodeshift";
+import jscodeshift from 'jscodeshift';
 
 /**
  * @typedef {import('../../types.js').Codemod} Codemod
@@ -10,38 +10,38 @@ import jscodeshift from "jscodeshift";
  * @returns {Codemod}
  */
 export default function (options) {
-  return {
-    name: "is-builtin-module",
-    transform: ({ file }) => {
-      const j = jscodeshift;
-      const root = j(file.source);
-      let dirtyFlag = false;
+	return {
+		name: 'is-builtin-module',
+		transform: ({ file }) => {
+			const j = jscodeshift;
+			const root = j(file.source);
+			let dirtyFlag = false;
 
-      // Replace import statement
-      root
-        .find(j.ImportDeclaration, {
-          source: { value: "is-builtin-module" },
-        })
-        .forEach((path) => {
-          path.value.source.value = "node:module";
-          path.value.specifiers = [
-            j.importSpecifier(j.identifier("isBuiltin")),
-          ];
-          dirtyFlag = true;
-        });
+			// Replace import statement
+			root
+				.find(j.ImportDeclaration, {
+					source: { value: 'is-builtin-module' },
+				})
+				.forEach((path) => {
+					path.value.source.value = 'node:module';
+					path.value.specifiers = [
+						j.importSpecifier(j.identifier('isBuiltin')),
+					];
+					dirtyFlag = true;
+				});
 
-      // Replace function calls
-      root
-        .find(j.CallExpression, {
-          callee: { name: "isBuiltinModule" },
-        })
-        .forEach((path) => {
-          // @ts-expect-error
-          path.value.callee.name = "isBuiltin";
-          dirtyFlag = true;
-        });
+			// Replace function calls
+			root
+				.find(j.CallExpression, {
+					callee: { name: 'isBuiltinModule' },
+				})
+				.forEach((path) => {
+					// @ts-expect-error
+					path.value.callee.name = 'isBuiltin';
+					dirtyFlag = true;
+				});
 
-      return dirtyFlag ? root.toSource() : file.source;
-    },
-  };
+			return dirtyFlag ? root.toSource() : file.source;
+		},
+	};
 }

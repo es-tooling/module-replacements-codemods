6c44b05cf68b99b61f57b408f37439c4d0f2b746
thepassle <pascalschilp@gmail.com>
2024-07-19 17:39:21 +0200

    chore: biome

--- scripts/which.js
+++ scripts/which.js
@@ -1,7 +1,13 @@
-import { nativeReplacements, microUtilsReplacements } from 'module-replacements';
+import {
+	nativeReplacements,
+	microUtilsReplacements,
+} from 'module-replacements';
 import { codemods } from '../codemods/index.js';
 
-const all = [...nativeReplacements.moduleReplacements, ...microUtilsReplacements.moduleReplacements];
+const all = [
+	...nativeReplacements.moduleReplacements,
+	...microUtilsReplacements.moduleReplacements,
+];
 
 /**
  * Figure out which module replacements are not implemented as codemods yet
@@ -16,15 +22,12 @@ filteredModuleReplacements.forEach((replacement) => {
 });
 
 const implementedCodemods = Object.keys(codemods).length; // Number of implemented codemods
-const implementedCodemodsPercentage =
-	(implementedCodemods / all.length) * 100;
+const implementedCodemodsPercentage = (implementedCodemods / all.length) * 100;
 
 console.log(
 	`\nNumber of implemented codemods: ${Object.keys(codemods).length}`,
 );
-console.log(
-	`Total number of module replacements: ${all.length}`,
-);
+console.log(`Total number of module replacements: ${all.length}`);
 console.log(
 	`Number of module replacements left to implement codemods for: ${filteredModuleReplacements.length}`,
 );

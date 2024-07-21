6c3b4512a78ddb21f52cd31778cef8c400616a8f
donecarlo <dancarlocanlas@gmail.com>
2024-07-21 20:27:27 +0800

    add codemod for function-bind

---
+++ codemods/function-bind/index.js
@@ -0,0 +1,24 @@
+import jscodeshift from 'jscodeshift';
+import { removeImport } from '../shared.js';
+
+/**
+ * @typedef {import('../../types.js').Codemod} Codemod
+ * @typedef {import('../../types.js').CodemodOptions} CodemodOptions
+ */
+
+/**
+ * @param {CodemodOptions} [options]
+ * @returns {Codemod}
+ */
+export default function(options) {
+  return {
+    name: 'function-bind',
+    transform: ({ file }) => {
+      const j = jscodeshift;
+      const root = j(file.source);
+			const { identifier: _ } = removeImport('function-bind', root, j);
+
+			return root.toSource(options);
+		},
+  }
+};

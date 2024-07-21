c29fea9b902950b5efa4db632b2ef04572f35542
Emily Medhurst <emily.medhurst2002@protonmail.com>
2024-07-21 19:30:42 +0100

    feat: add codemod

---
+++ codemods/get-symbol-description/index.js
@@ -0,0 +1,47 @@
+import jscodeshift from 'jscodeshift';
+import { removeImport } from '../shared.js';
+
+/**
+ * @typedef {import('../../types.js').Codemod} Codemod
+ * @typedef {import('../../types.js').CodemodOptions} CodemodOptions
+ */
+
+/**
+ * @param {CodemodOptions} [options]
+ * @returns {Codemod}
+ */
+export default function (options) {
+	return {
+		name: 'get-symbol-description',
+		transform: ({ file }) => {
+			const j = jscodeshift;
+			const root = j(file.source);
+			let dirtyFlag = false;
+
+			const { identifier } = removeImport('get-symbol-description', root, j);
+
+			root
+				.find(j.CallExpression, {
+					callee: {
+						type: 'Identifier',
+						name: identifier,
+					},
+				})
+				.forEach((path) => {
+					const args = path.value.arguments;
+					if (args.length === 1) {
+						const newExpression = j.memberExpression(
+							//@ts-ignore
+							args[0],
+							j.identifier('description'),
+						);
+
+						j(path).replaceWith(newExpression);
+						dirtyFlag = true;
+					}
+				});
+
+			return dirtyFlag ? root.toSource(options) : file.source;
+		},
+	};
+}

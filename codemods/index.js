39081f7d4cf82d708f445b1a4602e7dfc5b9a4ca
Mateusz Kadlubowski <xeho91@pm.me>
2024-07-21 21:50:34 +0800

    feat: add `is-primitive`

--- codemods/index.js
+++ codemods/index.js
@@ -196,6 +196,8 @@ import stringPrototypeTrimright from './string.prototype.trimright/index.js';
 
 import stringRaw from './string.raw/index.js';
 
+import isPrimitive from './is-primitive/index.js';
+
 export const codemods = {
 	'is-whitespace': isWhitespace,
 	'is-array-buffer': isArrayBuffer,
@@ -296,4 +298,5 @@ export const codemods = {
 	'string.prototype.trimleft': stringPrototypeTrimleft,
 	'string.prototype.trimright': stringPrototypeTrimright,
 	'string.raw': stringRaw,
+	'is-primitive': isPrimitive,
 };

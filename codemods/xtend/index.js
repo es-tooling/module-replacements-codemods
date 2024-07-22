6ba20fcacd1b966951f4e6ec21439f74eb711180
donecarlo <dancarlocanlas@gmail.com>
2024-07-22 23:29:25 +0800

    fix formatting

--- codemods/xtend/index.js
+++ codemods/xtend/index.js
@@ -10,28 +10,28 @@ import { removeImport } from '../shared.js';
  * @param {CodemodOptions} [options]
  * @returns {Codemod}
  */
-export default function(options) {
-  return {
-    name: 'xtend',
-    transform: ({ file }) => {
-      const j = jscodeshift;
+export default function (options) {
+	return {
+		name: 'xtend',
+		transform: ({ file }) => {
+			const j = jscodeshift;
 			const root = j(file.source);
 
 			const { identifier } = removeImport('xtend', root, j);
 
-      root
-        .find(j.CallExpression, {
-          callee: {
-            name: identifier,
-          }
-        })
-        .replaceWith(({ node }) => {
-          return j.objectExpression(
-            node.arguments.map(arg => j.spreadElement(arg))
-          )
-        });
+			root
+				.find(j.CallExpression, {
+					callee: {
+						name: identifier,
+					},
+				})
+				.replaceWith(({ node }) => {
+					return j.objectExpression(
+						node.arguments.map((arg) => j.spreadElement(arg)),
+					);
+				});
 
-      return root.toSource(options);
-    },
-  }
-};
+			return root.toSource(options);
+		},
+	};
+}

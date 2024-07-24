e81d35b7c26d6a1198c9bc941d7e85c02a2d0178
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-07-24 21:14:32 +0300

    feat: add concat-map

---
+++ test/fixtures/concat-map/case-1/before.js
@@ -0,0 +1,9 @@
+var concatMap = require("concat-map");
+var assert = require("assert");
+
+var xs = [1, 2, 3, 4, 5, 6];
+var ys = concatMap(xs, function (x) {
+  return x % 2 ? [x - 0.1, x, x + 0.1] : [];
+});
+
+assert.deepEqual(ys, [0.9, 1, 1.1, 2.9, 3, 3.1, 4.9, 5, 5.1]);

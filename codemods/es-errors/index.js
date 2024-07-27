6fbead7d3205839136fdfdaf72e172094d39eb6f
Matvey Ryabchikov <35634442+ronanru@users.noreply.github.com>
2024-07-27 17:00:25 +0300

    feat: add es-errors

---
+++ codemods/es-errors/index.js
@@ -0,0 +1,49 @@
+import jscodeshift from 'jscodeshift';
+import { removeImport } from '../shared.js';
+
+/**
+ * @typedef {import('../../types.js').Codemod} Codemod
+ * @typedef {import('../../types.js').CodemodOptions} CodemodOptions
+ */
+
+const moduleToErrorMap = {
+	'es-errors': 'Error',
+	'es-errors/eval': 'EvalError',
+	'es-errors/range': 'RangeError',
+	'es-errors/ref': 'ReferenceError',
+	'es-errors/syntax': 'SyntaxError',
+	'es-errors/type': 'TypeError',
+	'es-errors/uri': 'URIError',
+};
+
+/**
+ * @param {CodemodOptions} [options]
+ * @returns {Codemod}
+ */
+export default function (options) {
+	return {
+		name: 'es-errors',
+		transform: ({ file }) => {
+			const j = jscodeshift;
+			const root = j(file.source);
+			let dirtyFlag = false;
+
+			for (const [moduleName, errorName] of Object.entries(moduleToErrorMap)) {
+				const { identifier } = removeImport(moduleName, root, j);
+
+				root
+					.find(j.NewExpression, {
+						callee: {
+							type: 'Identifier',
+							name: identifier,
+						},
+					})
+					.replaceWith(({ node }) => {
+						return j.newExpression(j.identifier(errorName), node.arguments);
+					});
+			}
+
+			return root.toSource(options);
+		},
+	};
+}

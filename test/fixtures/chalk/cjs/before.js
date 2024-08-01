c647de009e3f684f3abe8f39e1fbd62173cf5014
43081j <43081j@users.noreply.github.com>
2024-08-01 22:48:14 +0100

    feat: add chalk codemod
    Adds a codemod for moving from `chalk` to `picocolors`.
    
    This will throw if exports are encountered which picocolors doesn't
    provide. For example, as of writing this, picocolors doesn't support
    bright colours (but soon will). That means `chalk.redBright` will throw
    since picocolors has no `redBright` export.
    
    These patterns are supported:
    
    ```ts
    
    // Before
    chalk.red.bold(str);
    // After
    pc.red(pc.bold(str));
    
    // Before
    chalk.red`i am red`;
    // After
    pc.red(`i am red`);
    
    // Before
    chalk.red`i am red and ${chalk.bold('bold')}!`;
    // After
    pc.red(`i am red and ${pc.bold('bold')}!`);
    ```
    
    Some patterns will not be detected. For example:
    
    ```ts
    // Not detected since it isn't a call expression
    chalk.red;
    
    // Similarly not detected
    const red = chalk.red;
    red(str);
    ```

---
+++ test/fixtures/chalk/cjs/before.js
@@ -0,0 +1,11 @@
+const ch = require('chalk');
+
+ch.red();
+ch.red('im red');
+ch.red.bold('im red and bold');
+ch.red`im a red template`;
+ch.red.bold`im a red and bold template`;
+ch.red`im an interpolated ${val} template`;
+ch.red`i contain ${ch.bold('sub chalks')}`;
+ch.red`i contain ${ch.bold`sub templates`}`;
+ch.red`i contain chained ${ch.blue.bold('sub chalks')}`;

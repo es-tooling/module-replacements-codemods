72f809e70d9ba653e247c15a082fa0efb4d8a36b
Puru Vijay <awesomepuruvj@gmail.com>
2024-08-02 19:24:25 +0530

    Push

---
+++ codemods/traverse/index.js
@@ -0,0 +1,40 @@
+import jscodeshift from 'jscodeshift';
+
+/**
+ * @typedef {import('../../types.js').Codemod} Codemod
+ * @typedef {import('../../types.js').CodemodOptions} CodemodOptions
+ */
+
+/**
+ * @param {CodemodOptions} [options]
+ * @returns {Codemod}
+ */
+export default function (options) {
+	return {
+		name: 'traverse',
+		transform: ({ file }) => {
+			const j = jscodeshift;
+			const root = j(file.source);
+
+			// Transform import declarations
+			root.find(j.ImportDeclaration).forEach((path) => {
+				if (j.Literal.check(path.node.source) && path.node.source.value === 'traverse') {
+					path.node.source.value = 'neotraverse/legacy';
+				}
+			});
+
+			// Transform require statements
+			root.find(j.CallExpression, { callee: { name: 'require' } }).forEach((path) => {
+				if (
+					path.node.arguments.length === 1 &&
+					j.Literal.check(path.node.arguments[0]) &&
+					path.node.arguments[0].value === 'traverse'
+				) {
+					path.node.arguments[0].value = 'neotraverse/legacy';
+				}
+			});
+
+			return root.toSource();
+		},
+	};
+}

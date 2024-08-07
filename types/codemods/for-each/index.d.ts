f5f2b4562561264cace7ebe28a90a11bb6e2b36b
thepassle <pascalschilp@gmail.com>
2024-08-07 17:51:19 +0200

    chore: format

--- types/codemods/for-each/index.d.ts
+++ types/codemods/for-each/index.d.ts
@@ -12,6 +12,8 @@
  * @param {CodemodOptions} [options]
  * @returns {Codemod}
  */
-export default function _default(options?: import("../../types.js").CodemodOptions | undefined): Codemod;
-export type Codemod = import("../../types.js").Codemod;
-export type CodemodOptions = import("../../types.js").CodemodOptions;
+export default function _default(
+	options?: import('../../types.js').CodemodOptions | undefined,
+): Codemod;
+export type Codemod = import('../../types.js').Codemod;
+export type CodemodOptions = import('../../types.js').CodemodOptions;
--- types/codemods/shared.d.ts
+++ types/codemods/shared.d.ts
@@ -11,14 +11,22 @@
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns {RemoveImport}
  */
-export function removeImport(name: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): RemoveImport;
+export function removeImport(
+	name: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): RemoveImport;
 /**
  * @param {string} code - code to insert after the last import
  * @param {import("jscodeshift").Collection} root - jscodeshift tree of the file containing the import
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  *
  */
-export function insertAfterImports(code: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): void;
+export function insertAfterImports(
+	code: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): void;
 /**
  * Analyzes an `import` or `require` statement to detect which names are being
  * imported and what identifiers is the developer assigning them to. Returns a
@@ -40,9 +48,13 @@ export function insertAfterImports(code: string, root: import("jscodeshift").Col
  * @param {import("jscodeshift").Collection} root - jcodeshift tree of the file containing the import
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  */
-export function getImportIdentifierMap(packageName: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): Record<string, string | undefined> & {
-    [DEFAULT_IMPORT]?: string;
-    [NAMESPACE_IMPORT]?: string;
+export function getImportIdentifierMap(
+	packageName: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): Record<string, string | undefined> & {
+	[DEFAULT_IMPORT]?: string;
+	[NAMESPACE_IMPORT]?: string;
 };
 /**
  * Replaces import declarations that use default specifiers
@@ -59,7 +71,13 @@ export function getImportIdentifierMap(packageName: string, root: import("jscode
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns {ReplaceDefaultImport}
  */
-export function replaceDefaultImport(name: string, newSpecifier: string, newName: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): ReplaceDefaultImport;
+export function replaceDefaultImport(
+	name: string,
+	newSpecifier: string,
+	newName: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): ReplaceDefaultImport;
 /**
  * @param {string} method - e.g. `array.prototype.flatMap`
  * @param {string} identifierName - e.g. `flatMap`
@@ -67,7 +85,12 @@ export function replaceDefaultImport(name: string, newSpecifier: string, newName
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns {boolean} - true if the method was found and transformed, false otherwise
  */
-export function transformArrayMethod(method: string, identifierName: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): boolean;
+export function transformArrayMethod(
+	method: string,
+	identifierName: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  * @param {import("jscodeshift").ASTPath<import("jscodeshift").CallExpression>} path -  jscodeshift path
  * @param {string} instanceName - e.g. `Uint8Array`
@@ -75,7 +98,12 @@ export function transformArrayMethod(method: string, identifierName: string, roo
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns
  */
-export function transformInstanceProperty(path: import("jscodeshift").ASTPath<import("jscodeshift").CallExpression>, instanceName: string, propertyName: string, j: import("jscodeshift").JSCodeshift): boolean;
+export function transformInstanceProperty(
+	path: import('jscodeshift').ASTPath<import('jscodeshift').CallExpression>,
+	instanceName: string,
+	propertyName: string,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  * @param {import("jscodeshift").ASTPath<import("jscodeshift").CallExpression>} path -  jscodeshift path
  * @param {string} instanceName - e.g. `Uint8Array`
@@ -83,7 +111,12 @@ export function transformInstanceProperty(path: import("jscodeshift").ASTPath<im
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns
  */
-export function transformInstanceMethod(path: import("jscodeshift").ASTPath<import("jscodeshift").CallExpression>, instanceName: string, methodName: string, j: import("jscodeshift").JSCodeshift): boolean;
+export function transformInstanceMethod(
+	path: import('jscodeshift').ASTPath<import('jscodeshift').CallExpression>,
+	instanceName: string,
+	methodName: string,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  * @param {string} method - e.g. `array.prototype.flatMap`
  * @param {string} identifierName - e.g. `flatMap`
@@ -91,7 +124,12 @@ export function transformInstanceMethod(path: import("jscodeshift").ASTPath<impo
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns {boolean} - true if the method was found and transformed, false otherwise
  */
-export function transformStringMethod(method: string, identifierName: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): boolean;
+export function transformStringMethod(
+	method: string,
+	identifierName: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  * @param {string} importName - e.g. `math.acosh/polyfill`
  * @param {string} methodName - e.g. `acosh`
@@ -99,28 +137,47 @@ export function transformStringMethod(method: string, identifierName: string, ro
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  * @returns {boolean} - true if the method was found and transformed, false otherwise
  */
-export function transformMathPolyfill(importName: string, methodName: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): boolean;
+export function transformMathPolyfill(
+	importName: string,
+	methodName: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  * @param {string} importName = e.g., `define-properties`
  * @param {string} identifier = e.g., `supportsDescriptors`
  * @param {import("jscodeshift").Collection} root
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  */
-export function getVariableExpressionHasIdentifier(importName: string, identifier: string, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): boolean;
+export function getVariableExpressionHasIdentifier(
+	importName: string,
+	identifier: string,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  * @param {string} importName = e.g., `define-properties`
  * @param {string | boolean | null | number | RegExp} value = e.g., true or "string value"
  * @param {import("jscodeshift").Collection} root
  * @param {import("jscodeshift").JSCodeshift} j - jscodeshift instance
  */
-export function replaceRequireMemberExpression(importName: string, value: string | boolean | null | number | RegExp, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): boolean;
+export function replaceRequireMemberExpression(
+	importName: string,
+	value: string | boolean | null | number | RegExp,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): boolean;
 /**
  *
  * @param {number} line
  * @param {import("jscodeshift").Collection} root
  * @param {import("jscodeshift").JSCodeshift} j
  */
-export function getAncestorOnLine(line: number, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): any;
+export function getAncestorOnLine(
+	line: number,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): any;
 /**
  *
  * @param {import("jscodeshift").CommentBlock} comment
@@ -128,24 +185,29 @@ export function getAncestorOnLine(line: number, root: import("jscodeshift").Coll
  * @param {import("jscodeshift").Collection} root
  * @param {import("jscodeshift").JSCodeshift} j
  */
-export function insertCommentAboveNode(comment: import("jscodeshift").CommentBlock, startLine: number, root: import("jscodeshift").Collection, j: import("jscodeshift").JSCodeshift): void;
+export function insertCommentAboveNode(
+	comment: import('jscodeshift').CommentBlock,
+	startLine: number,
+	root: import('jscodeshift').Collection,
+	j: import('jscodeshift').JSCodeshift,
+): void;
 export const DEFAULT_IMPORT: unique symbol;
 export const NAMESPACE_IMPORT: unique symbol;
 /**
  * type definition for return type object
  */
 export type RemoveImport = {
-    /**
-     * - the name of the module as it was imported or required. for example, `keys` in `import keys from 'object-keys'`
-     */
-    identifier: string;
+	/**
+	 * - the name of the module as it was imported or required. for example, `keys` in `import keys from 'object-keys'`
+	 */
+	identifier: string;
 };
 /**
  * type definition for return type object
  */
 export type ReplaceDefaultImport = {
-    /**
-     * - the name of the module as it was imported or required. for example, `keys` in `import keys from 'object-keys'`
-     */
-    identifier: string;
+	/**
+	 * - the name of the module as it was imported or required. for example, `keys` in `import keys from 'object-keys'`
+	 */
+	identifier: string;
 };

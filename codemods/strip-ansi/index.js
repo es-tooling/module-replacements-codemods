0b8e98d26c7cead254dd1e82b51146b4ada1ef39
Cristopher <cristophernamchee12@gmail.com>
2024-11-15 21:59:45 +0700

    [WIP] feat: add strip-ansi codemod

---
+++ codemods/strip-ansi/index.js
@@ -0,0 +1,21 @@
+import jscodeshift from 'jscodeshift';
+
+/**
+ * @typedef {import('../../types.js').Codemod} Codemod
+ * @typedef {import('../../types.js').CodemodOptions} CodemodOptions
+ */
+
+/**
+ * @param {CodemodOptions} [options]
+ * @returns {Codemod}
+ */
+export default function (options) {
+	return {
+		name: 'strip-ansi',
+		transform: ({ file }) => {
+			const j = jscodeshift;
+			const root = j(file.source);
+
+		},
+	};
+}

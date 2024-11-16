b5d6527b5c5465b836de7503a8319fab2e7f76da
Cristopher <cristophernamchee12@gmail.com>
2024-11-16 21:49:59 +0700

    feat: add strip-ansi codemod

--- codemods/strip-ansi/index.js
+++ codemods/strip-ansi/index.js
@@ -15,7 +15,38 @@ export default function (options) {
 		transform: ({ file }) => {
 			const j = jscodeshift;
 			const root = j(file.source);
+			let dirtyFlag = false;
 
+			const importDeclarations = root.find(j.ImportDeclaration, {
+				source: {
+					value: 'strip-ansi',
+				},
+			});
+
+			let importName = 'stripAnsi';
+
+			for (const declaration of importDeclarations) {
+				const name = declaration.value.specifiers?.[0].local?.name;
+				if (name) {
+					importName = name;
+				}
+				path.value.source.value = 'node:util';
+				path.value.specifiers = [j.importSpecifier(j.identifier('stripVTControlCharacters'))];
+				dirtyFlag = true;
+			}
+
+			const callExpressions = root.find(j.CallExpression, {
+				callee: {
+					name: importName,
+				}
+			});
+
+			for (const expression of callExpressions) {
+				expression.value.callee.name = 'stripVTControlCharacters';
+				dirtyFlag = true;
+			}
+
+			return dirtyFlag ? root.toSource() : file.source;
 		},
 	};
 }

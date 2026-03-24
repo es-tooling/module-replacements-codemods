6096b0214a9773c91899be57439ba3a66f5ba719
Carl Olsen <unstoppablecarlolsen@gmail.com>
2026-03-24 13:07:36 -0400

    Add bright color options to chalk codemod

--- codemods/chalk/index.js
+++ codemods/chalk/index.js
@@ -27,6 +27,24 @@ const picoNames = [
 	'bgMagenta',
 	'bgCyan',
 	'bgWhite',
+	
+	'blackBright', 
+	'redBright', 
+	'greenBright', 
+	'yellowBright', 
+	'blueBright', 
+	'magentaBright', 
+	'cyanBright', 
+	'whiteBright', 
+	
+	'bgBlackBright', 
+	'bgRedBright', 
+	'bgGreenBright', 
+	'bgYellowBright', 
+	'bgBlueBright', 
+	'bgMagentaBright', 
+	'bgCyanBright', 
+	'bgWhiteBright', 
 ];
 
 /**

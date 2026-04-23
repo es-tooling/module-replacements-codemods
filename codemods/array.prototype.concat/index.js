20558411ef5d76063c174c397c15d9ed1f7a1f05
Roman <dev@rman.dev>
2026-04-23 21:18:53 +0100

    Update index.js

--- codemods/array.prototype.concat/index.js
+++ codemods/array.prototype.concat/index.js
@@ -38,7 +38,8 @@ export default function (options) {
 
 			for (const call of callExpressions) {
 				const argsMatch = call.getMultipleMatches('ARGS');
-				if (argsMatch) {
+				if (!argsMatch) continue;
+			
 					const args = argsMatch.filter((m) => m.kind() !== ',');
 					if (args.length >= 1) {
 						const arrayText = args[0].text();
@@ -48,7 +49,7 @@ export default function (options) {
 							.join(', ');
 						edits.push(call.replace(`${arrayText}.concat(${concatArgs})`));
 					}
-				}
+				
 			}
 
 			for (const imp of imports) {

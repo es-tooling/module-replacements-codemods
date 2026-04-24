9f8142c775bc1e5afd5caf81a7adcfc2eb6e1588
Roman <dev@rman.dev>
2026-04-24 11:43:35 +0100

    wip

--- test/fixtures/clone-regexp/case-2/before.js
+++ test/fixtures/clone-regexp/case-2/before.js
@@ -2,27 +2,18 @@ import cloneRegexp from 'clone-regexp';
 
 const regex = /[a-z]/gi;
 
-// Argument is a regex, with a `false` option
+// Argument is a regex
 const clone1 = cloneRegexp(/[a-z]/gi, { global: false });
 
-// Argument is a regex, with a `true` option
-const clone2 = cloneRegexp(/[a-z]/, { global: true });
-
-// Argument is an identifier, with a `true` option
-const clone3 = cloneRegexp(regex, { unicode: true });
-
-// Argument is an identifier, with a `false` option
-const clone4 = cloneRegexp(regex, { unicode: false });
-
-// Argument is regex, with several options
-const clone5 = cloneRegexp(/[a-z]/, { unicode: true, global: true });
+// Argument is an identifier
+const clone2 = cloneRegexp(regex, { unicode: true });
 
 // Both arguments are identifier
 const opt = { ignoreCase: 1 === 1 };
-const clone6 = cloneRegexp(regex, opt);
+const clone3 = cloneRegexp(regex, opt);
 
 // Multiple cloneRegexp
-const clone7 = f(cloneRegexp(regex, { sticky: true }), cloneRegexp(regex, { dotAll: true }));
+const clone4 = f(cloneRegexp(regex, { sticky: true }), cloneRegexp(regex, { dotAll: true }));
 
 // Regex comes from a function
-const clone8 = cloneRegexp(f(regex), { multiline: true });
+const clone5 = cloneRegexp(f(regex), { multiline: true });
--- test/fixtures/clone-regexp/case-2/result.js
+++ test/fixtures/clone-regexp/case-2/result.js
@@ -2,27 +2,18 @@
 
 const regex = /[a-z]/gi;
 
-// Argument is a regex, with a `false` option
+// Argument is a regex
 const clone1 = new RegExp(/[a-z]/gi);
 
-// Argument is a regex, with a `true` option
-const clone2 = new RegExp(/[a-z]/, 'g');
-
-// Argument is an identifier, with a `true` option
-const clone3 = new RegExp(regex, 'u');
-
-// Argument is an identifier, with a `false` option
-const clone4 = new RegExp(regex);
-
-// Argument is regex, with several options
-const clone5 = new RegExp(/[a-z]/);
+// Argument is an identifier
+const clone2 = new RegExp(regex, 'u');
 
 // Both arguments are identifier
 const opt = { ignoreCase: 1 === 1 };
-const clone6 = new RegExp(regex);
+const clone3 = new RegExp(regex);
 
 // Multiple cloneRegexp
-const clone7 = f(new RegExp(regex, 'y'), new RegExp(regex, 's'));
+const clone4 = f(new RegExp(regex, 'y'), new RegExp(regex, 's'));
 
 // Regex comes from a function
-const clone8 = new RegExp(f(regex), 'm');
+const clone5 = new RegExp(f(regex), 'm');

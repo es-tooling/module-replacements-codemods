a3b91e84c5b11b345e0da35f81b1e2d02234ce7f
Roman <dev@rman.dev>
2026-04-27 23:15:42 +0100

    Update index.js

--- codemods/is-builtin-module/index.js
+++ codemods/is-builtin-module/index.js
@@ -2,6 +2,12 @@ import jscodeshift from 'jscodeshift';
 
 /**
  * @typedef {import('../../types.js').Codemod} Codemod
+ * @typedef {import('../../types.js').CodemodOptions} CodemodOptions
+ */
+
+/**
+ * @param {CodemodOptions} [options]
+ * @returns {Codemod}
  */
 export default function (options) {
 	return {

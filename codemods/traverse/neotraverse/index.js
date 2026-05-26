c3a5a7b9e7cfa0dfcd9c1e376291e319192c35ff
Roman <dev@rman.dev>
2026-05-26 08:45:21 +0100

    feat: migrate `traverse` to ast-grep

--- codemods/traverse/neotraverse/index.js
+++ codemods/traverse/neotraverse/index.js
@@ -1,4 +1,7 @@
-import jscodeshift from 'jscodeshift';
+import { ts } from '@ast-grep/napi';
+import { replaceDefaultImport } from '../../shared-ast-grep.js';
+
+const MODULE_NAME = 'traverse';
 
 /**
  * @typedef {import('../../../types.js').Codemod} Codemod
@@ -11,36 +14,19 @@ import jscodeshift from 'jscodeshift';
  */
 export default function (options) {
 	return {
-		name: 'traverse',
+		name: MODULE_NAME,
 		to: 'neotraverse',
 		transform: ({ file }) => {
-			const j = jscodeshift;
-			const root = j(file.source);
+			const ast = ts.parse(file.source);
+			const root = ast.root();
 
-			// Transform import declarations
-			root.find(j.ImportDeclaration).forEach((path) => {
-				if (
-					j.Literal.check(path.node.source) &&
-					path.node.source.value === 'traverse'
-				) {
-					path.node.source.value = 'neotraverse/legacy';
-				}
-			});
+			const { edits } = replaceDefaultImport(
+				root,
+				MODULE_NAME,
+				'neotraverse/legacy',
+			);
 
-			// Transform require statements
-			root
-				.find(j.CallExpression, { callee: { name: 'require' } })
-				.forEach((path) => {
-					if (
-						path.node.arguments.length === 1 &&
-						j.Literal.check(path.node.arguments[0]) &&
-						path.node.arguments[0].value === 'traverse'
-					) {
-						path.node.arguments[0].value = 'neotraverse/legacy';
-					}
-				});
-
-			return root.toSource({ quote: 'single' });
+			return root.commitEdits(edits);
 		},
 	};
 }
